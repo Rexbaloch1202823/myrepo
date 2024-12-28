567552302a9bc8a1c88c0e519b2a85de6866fa68
shayakkubdani <mirshayak5@gmail.com>
2024-12-28 07:29:50 +0000

    version 0.2

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import cv2 as cv
-import numpy as numpy
+import numpy as np
 
 img = cv.imread('srk.jpg')
-img = cv.resize(img,(500,600))
+img = cv.resize(img,(500,500))
 gray_img = cv.GaussianBlur(img, (7,7,),0)
-cv.imshow('gray', gray_img)
+h = np.hstack(img, gray_img)
+v = np.vstack(h)
+cv.imshow('gray', v)
 cv.waitKey(0)
 cv.destroyAllWindows()
